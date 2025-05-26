79c18691bf14c1e1f5876bac520a49e1e94e21fd
minhphat1612 <minhphat1612@gmail.com>
2025-05-26 18:59:05 +0700

    Change

---
+++ WebStoryService/Areas/MyApi/MyApiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebStoryService.Areas.MyApi
+{
+    public class MyApiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MyApi";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MyApi_default",
+                "MyApi/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebStoryService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebStoryService.MvcApplication" Language="C#" %>
