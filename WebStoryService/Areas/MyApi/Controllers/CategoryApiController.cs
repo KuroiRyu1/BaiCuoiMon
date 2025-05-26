ab7bb16e8f14367e7f959546b31893af130cabf0
minhphat1612 <minhphat1612@gmail.com>
2025-05-26 19:45:33 +0700

    change

--- WebStoryService/Areas/MyApi/Controllers/CategoryApiController.cs
+++ WebStoryService/Areas/MyApi/Controllers/CategoryApiController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using WebStoryService.Models.ModelData;
+using WebStoryService.Models.Repositories;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,7 +16,31 @@ namespace WebStoryService.Areas.MyApi.Controllers
         [HttpGet]
         public IEnumerable<Models.ModelData.Category> Get()
         {
+            var headerData = Request.Headers;
+            string username = string.Empty;
+            string password = string.Empty;
+            string token = string.Empty;
+            if (headerData.Contains("username"))
+            {
+                {
+                    username = headerData.GetValues("username").First();
+                }
+                if (headerData.Contains("pwd"))
+                {
+                    password = headerData.GetValues("pwd").First();
+                }
+                if (headerData.Contains("tk"))
+                {
+                    token = headerData.GetValues("tk").First();
+                }
+                if (AccountRep.checkToken(username, password, token) == true)
+                {
+                    CategoryRes res = new CategoryRes();
+                    return res.Gets();
+                }
+            }
 
+            return new List<Category>();
         }
     }
 }
---
+++ WebStoryService/Models/Repositories/CategoryRes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebStoryService.Models.Entity;
+using WebStoryService.Models.ModelData;
+
+namespace WebStoryService.Models.Repositories
+{
+    public class CategoryRes
+    {
+        public List<Category> Gets()
+        {
+            List<Category> list = new List<Category>();
+            try
+            {
+                DbEntities entities = new DbEntities();
+                list = entities.tbl_category.Select(d => new Category
+                {
+                    Id = d.C_id,
+                    Name = d.C_name,
+                    Active = d.C_active ?? 0,
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+            return list;
+        }
+    }
+}
