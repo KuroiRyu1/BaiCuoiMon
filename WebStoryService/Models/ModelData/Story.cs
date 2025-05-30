45cbfc5ee72453986e042100cb658b1dd73ac445
Duy Anh <150674221+duyanhhhhhn@users.noreply.github.com>
2025-05-30 18:56:34 +0700

    add model story

---
+++ WebStoryService/Models/ModelData/Story.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStoryService.Models.ModelData
+{
+    public class Story
+    {
+        public int Id { get; set; } = 0;
+
+        public string Title { get; set; } = "";
+        public int ChapterNumber { get; set; } = 1;
+        public string Introduction { get; set; } = "";
+        public string Image { get; set; } = "default.jpg";
+        public int LikeNumber { get; set; } = 0;
+        public int FollowNumber { get; set; } = 0;
+        public int ViewNumber { get; set; } = 0;
+
+        public int AuthorId { get; set; } = 0;
+        public int StatusId { get; set; } = 0;
+        public int CategoryId { get; set; } = 0;
+        public int StoryTypeId { get; set; } = 0;
+        public int ScheduleId { get; set; } = 0;
+    }
+}
