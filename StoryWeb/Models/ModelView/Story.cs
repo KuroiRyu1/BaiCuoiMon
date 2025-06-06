c8f0d80df2866be39b8220c25c8770cce7db1f63
Lân Octavian <lantruongtuan333@gmail.com>
2025-06-06 23:52:36 +0700

    Story UI(scratch)

---
+++ StoryWeb/Models/ModelView/Story.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoryWeb.Models.ModelView
+{
+    public class Story
+    {
+        public int Id { get; set; } = 0;
+        public string Title { get; set; } = "";
+        public int ChapterNumber { get; set; } = 1;
+        public string Introduction { get; set; } = "";
+        public string Image { get; set; } = "default.jpg";
+        public int LikeNumber { get; set; } = 0;
+        public int FollowNumber { get; set; } = 0;
+        public decimal ViewNumber { get; set; } = 0;
+        public int AuthorId { get; set; } = 0;
+        public int StatusId { get; set; } = 0;
+        public int CategoryId { get; set; } = 0;
+        public int StoryTypeId { get; set; } = 0;
+        public string AuthorName { get; set; } = "";
+        public string CategoryName { get; set; } = "";
+    }
+}
